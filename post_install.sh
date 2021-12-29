6375d0243408ceca0d9279567102d55cc4f01684
Richard <richard2891@gmail.com>
2021-12-28 20:56:42 -0500

    update post_install to use prowlarr from package manager.

--- post_install.sh
+++ post_install.sh
@@ -1,14 +1,13 @@
 #!/bin/sh -x
 IP_ADDRESS=$(ifconfig | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}')
 
-fetch https://github.com/Thefrank/freebsd-port-sooners/releases/download/20211015/prowlarrdotnet-0.1.1.978.pkg
+sysrc prowlarr_enable=TRUE
 
-pkg install -y prowlarrdotnet-0.1.1.978.pkg
-rm prowlarrdotnet-0.1.1.978.pkg
+chown -R prowlarr:prowlarr /usr/local/share/prowlarr
 
-sysrc prowlarrdotnet_enable=TRUE
+chmod 755 /usr/local/prowlarr
 
-service prowlarrdotnet start
+service prowlarr start
 
 echo -e "Prowlarr now installed.\n" > /root/PLUGIN_INFO
 echo -e "\nPlease open your web browser and go to http://${IP_ADDRESS}:9696 to configure Prowlarr.\n" >> /root/PLUGIN_INFO
